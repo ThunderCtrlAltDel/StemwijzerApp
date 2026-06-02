5b420701d5bfc5692cebb416602435f0416897b6
Ronan <heerbderoo@gmail.com>
2026-06-02 12:30:38 +0200

    Layout partijenpage

--- StemwijzerApp/Pages/PartijenPage.xaml.cs
+++ StemwijzerApp/Pages/PartijenPage.xaml.cs
@@ -11,5 +11,18 @@ namespace StemwijzerApp.Pages
         {
             InitializeComponent();
         }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+
+    public class VoorbeeldPartij
+    {
+        public string Afkorting { get; set; }
+        public string Naam { get; set; }
+        public string Beschrijving { get; set; }
+        public string HexKleur { get; set; }
     }
 }
